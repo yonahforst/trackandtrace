f9191753868f4d0b12fac21c61a9eff800491b4d
Yonah Forst <yonaforst@hotmail.com>
2020-03-24 22:10:08 +0100

    open settings if permission was denied

--- components/PermissionCard.js
+++ components/PermissionCard.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 import {
-  StyleSheet
+  StyleSheet,
+  Linking,
+  Alert,
 } from 'react-native'
 
 import { 
@@ -10,43 +12,84 @@ import {
   Card, 
   Title, 
   Paragraph,
+  HelperText,
 } from 'react-native-paper';
 
 import i18n from 'i18n-js'
+
+
+const alertOpenSettings = (title, body) => {
+  Alert.alert(
+    title,
+    body,
+    [
+      {
+        text: i18n.t('cancel'),
+        onPress: () => {},
+        style: 'cancel',
+      },
+      { text: i18n.t('open_settings'), onPress: () => Linking.openURL('app-settings:') },
+    ],
+  )
+}
 
 export default ({
   title,
   subtitle,
   content,
   icon,
-  hasPermission,
+  permission,
   onRequestPermission,
-}) => (
-  <Card>
-    <Card.Title 
-      title={title} 
-      subtitle={subtitle} 
-      left={(props) => <Avatar.Icon {...props} icon={icon} />} 
-      />
-    <Card.Content>
-      <Paragraph>
-        { content }
-      </Paragraph>
-    </Card.Content>
-    <Card.Actions
-    style={styles.actions}>
+}) => {
+  const renderButtonForStatus = () => {
 
+     if (permission.status == 'granted')
+        return (
+          <Button 
+          icon={'check'} 
+          mode="contained" 
+          disabled={true}>
+            { i18n.t('intro_permission_granted') }
+          </Button>
+        )
+
+    if (permission.status == 'undetermiend' || permission.canAskAgain) 
+      return (
+        <Button 
+        mode="contained" 
+        onPress={onRequestPermission}>
+          { i18n.t('intro_permission_ask') }
+        </Button>
+      )
+        
+    return (
       <Button 
-      icon={hasPermission ? 'check' : undefined} 
-      mode="contained" 
-      disabled={hasPermission}
-      onPress={onRequestPermission}>
-        {hasPermission ? i18n.t('intro_permission_granted') : i18n.t('intro_permission_ask')}
+      mode="contained"
+      onPress={() => alertOpenSettings(title, i18n.t('intro_permission_denied'))}>
+        { i18n.t('intro_permission_ask') }
       </Button>
+    )
+  }
 
-    </Card.Actions>
-  </Card>
-);
+  return (
+    <Card>
+      <Card.Title 
+        title={title} 
+        subtitle={subtitle} 
+        left={(props) => <Avatar.Icon {...props} icon={icon} />} 
+        />
+      <Card.Content>
+        <Paragraph>
+          { content }
+        </Paragraph>
+      </Card.Content>
+      <Card.Actions
+      style={styles.actions}>
+        { renderButtonForStatus() }
+      </Card.Actions>
+    </Card>
+  )
+}
 
 const styles = StyleSheet.create({
   actions: {
